04322fea8becb64c375bc7babb3faf99fa661b0e
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-02-09 14:19:33 -0800

    Tests

--- app/src/main/java/com/example/mediastoreaccessor/AudioFileAccessor.kt
+++ app/src/main/java/com/example/mediastoreaccessor/AudioFileAccessor.kt
@@ -1,4 +1,79 @@
 package com.example.mediastoreaccessor
 
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import java.util.*
+
 class AudioFileAccessor {
+    // Container for information about each audio file
+    data class AudioFile(
+        val uri: Uri,
+        val title: String,
+        val data: String,
+        val mimeType: String
+    )
+    /*
+        make an example code that creates + saves file -> read it
+     */
+
+    fun getAudioFiles(contentResolver: ContentResolver): List<AudioFile> {
+
+        val audioFiles = mutableListOf<AudioFile>()
+
+        val collection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.MIME_TYPE
+        )
+
+        // Select audio files with the mp4 extension (recorded files on AudioMoth end with .mp4)
+        val selection = "${MediaStore.Audio.Media.MIME_TYPE} = ?"
+        val selectionArgs = arrayOf("audio/wav")
+
+        val query = contentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArgs,
+            null
+        )
+
+        // Add error handling in case the query is null
+        if (query == null) {
+            Log.e("AudioFileAccessor", "Query returned null.")
+            return audioFiles
+        }
+
+        query?.use {
+            val idColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val dataColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+            val mimeTypeColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.MIME_TYPE)
+
+            while (it.moveToNext()) {
+                // Get values of column for a given audio file
+                val id = it.getLong(idColumn)
+                val title = it.getString(titleColumn)
+                val data = it.getString(dataColumn)
+                Log.d("Data Column:", data)
+                val mimeType = it.getString(mimeTypeColumn)
+
+                val audioUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id
+                )
+
+                audioFiles.add(AudioFile(audioUri, title, data, mimeType))
+            }
+            Log.d("AudioFileAccessor", "Number of audio files after loop: ${audioFiles.size}")
+            it.close()
+        }
+
+        return audioFiles
+    }
 }
--- app/src/main/java/com/example/mediastoreaccessor/MainActivity.kt
+++ app/src/main/java/com/example/mediastoreaccessor/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.mediastoreaccessor
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.appsearch.SetSchemaRequest.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSION_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Request the permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE)
+        } else {
+            // Permission has already been granted
+            var audioFileAccessor = AudioFileAccessor()
+            var audioFiles = audioFileAccessor.getAudioFiles(contentResolver)
+            var audioTextView = findViewById<TextView>(R.id.audioFileTextView)
+            Log.i("Audio File Size:", audioFiles.size.toString())
+            var audioFileText = ""
+            if (audioFiles.isEmpty()) {
+                audioTextView.text = "No audio files found in the media store."
+            } else {
+                for (audioFile in audioFiles) {
+                    audioFileText += audioFile.data + "/n "
+                }
+                audioTextView.text = audioFileText
+            }
+        }
     }
+
 }
